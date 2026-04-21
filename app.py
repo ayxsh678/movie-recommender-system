f32b71a6f468a7eaadd8a0632d42010560e1553c
Ayush Verma <148683764+ayxsh678@users.noreply.github.com>
2026-04-21 22:20:34 +0530

    fix: compute similarity dynamically instead of loading pickle

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pickle
 
 movies = pickle.load(open('movies.pkl','rb'))
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 cv = CountVectorizer(max_features=5000, stop_words='english')
 vectors = cv.fit_transform(movies['tags']).toarray()
